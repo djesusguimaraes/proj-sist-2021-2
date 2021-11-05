458df72dcf47ef8ae0ae07505215974c9cb6198a
Max Alexandre <max.alexandre839@gmail.com>
2021-11-05 00:08:56 -0300

    R10 | Correção | T03 | Redirecionamento
     - Corrigir a bind que estava errada, ainda vai precisar ser atualizada.

--- lib/app/modules/editdata_client/editdata_module.dart
+++ lib/app/modules/editdata_client/editdata_module.dart
@@ -1,6 +1,3 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:pscomidas/app/modules/auth/auth_repository.dart';
-import 'package:pscomidas/app/modules/auth/auth_store.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 import 'package:pscomidas/app/modules/edit_data_client_page.dart';
@@ -9,8 +6,7 @@ class EditDataClientModule extends Module {
   static String get routeName => '/edit';
   @override
   final List<Bind> binds = [
-    Bind.lazySingleton((i) => AuthRepository(FirebaseAuth.instance)),
-    Bind.lazySingleton((i) => AuthStore(i.get())),
+    Bind.lazySingleton((i) => EditDataRepository()),
   ];
 
   @override
