b1167daf431160572c005cc3a29e7e6af193a4e8
djesusguimaraes <jesus.daniel@mail.uft.edu.br>
2021-12-11 16:07:48 -0300

    Merge branch 'origin develop' into bugfix/app-session

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -2,7 +2,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:pscomidas/app/global/repositories/products/product_repository.dart';
-import 'package:pscomidas/app/global/utils/auth_guard.dart';
 import 'package:pscomidas/app/modules/auth/auth_module.dart';
 import 'package:pscomidas/app/modules/auth/auth_repository.dart';
 import 'package:pscomidas/app/modules/cart/cart_module.dart';
